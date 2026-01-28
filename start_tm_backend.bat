78282bcdc3c134d72d9db9159c73080d3df6e95a
11mia1717 <11mia1717@gmail.com>
2026-01-28 14:25:49 +0900

    Fix: Use absolute paths in TM Center startup scripts for environment stability

--- start_tm_backend.bat
+++ start_tm_backend.bat
@@ -1,6 +1,4 @@
 @echo off
 echo Starting TM Center Backend on port 8082...
 cd tm-center-tossbank\webapp\backend
-set MAVEN_HOME=C:\apache-maven-3.8.8
-set PATH=%MAVEN_HOME%\bin;%PATH%
-mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=8082
+"C:\Program Files\Apache\maven\apache-maven-3.9.12-bin\bin\mvn.cmd" spring-boot:run -Dspring-boot.run.arguments=--server.port=8082
--- start_tm_frontend.bat
+++ start_tm_frontend.bat
@@ -3,4 +3,4 @@ echo Starting TM Center Frontend on port 5178...
 cd tm-center-tossbank\webapp\frontend
 set VITE_PORT=5178
 set VITE_BACKEND_URL=http://localhost:8082
-npm run dev
+"C:\Program Files\nodejs\npm.cmd" run dev
